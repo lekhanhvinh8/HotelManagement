fa3e0a0d32d81c8cde2815a85eb5e4572adfe3cd
Hung Do <beekunfar@gmai.com>
2021-01-07 11:57:03 +0700

    complete invoice

---
+++ HotelManagement/HotelManagement/Models/ViewModels/InvoiceModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+
+namespace HotelManagement.Models.ViewModels
+{
+    public class InvoiceModel
+    {
+        private HotelManagementDbContext _context = null;
+        public InvoiceModel()
+        {
+            _context = new HotelManagementDbContext();
+        }
+        public bool AddGuest(Guest guest)
+        {
+            try
+            {
+                _context.Guests.Add(guest);
+                _context.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool UpdateGuest(Guest guest, string name, bool gender, string address, int cat)
+        {
+            try
+            {
+                guest.Name = name;
+                guest.Sex = gender;
+                guest.Address = address;
+                guest.GuestCategoryId = cat;
+                _context.Guests.AddOrUpdate(guest);
+                _context.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool AddInvoice(string invoiceID, int guestId, float totalCost)
+        {
+            try
+            {
+                var invoice = new Invoice();
+                invoice.ID = invoiceID;
+                invoice.GuestId = guestId;
+                invoice.DateOfInvoice = DateTime.Now;
+                invoice.TotalCost = totalCost;
+                _context.Invoices.Add(invoice);
+                _context.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool UpdateRoomRentalSlip(RoomRentalSlip roomRentalSlip)
+        {
+            try
+            {            
+                _context.RoomRentalSlips.AddOrUpdate(roomRentalSlip);
+                _context.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
