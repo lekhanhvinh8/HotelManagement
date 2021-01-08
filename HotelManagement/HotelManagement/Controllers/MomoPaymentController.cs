26c963d99ab330a85b117eb1a240bec9fcd98463
Vinh <dotkichvz58@gmail.com>
2021-01-08 07:43:01 +0700

    impelement momo payment

--- HotelManagement/HotelManagement/Controllers/MomoPaymentController.cs
+++ HotelManagement/HotelManagement/Controllers/MomoPaymentController.cs
@@ -1,4 +1,5 @@
 ﻿using HotelManagement.Models;
+using HotelManagement.Models.Dtos;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
@@ -20,9 +21,9 @@ namespace HotelManagement.Controllers
         {
             //request params need to request to MoMo system
             string endpoint = @"https://test-payment.momo.vn/gw_payment/transactionProcessor";
-            string partnerCode = "MOMOIQA420180417";
-            string accessKey = "SvDmj2cOTYZmQQ3H";
-            string serectkey = "PPuDXq1KowPT1ftR8DvlQTHhC03aul17";
+            string partnerCode = MomoConfig.ParnerCode;
+            string accessKey = MomoConfig.AccessKey;
+            string serectkey = MomoConfig.SecretKey;
 
             string orderInfo = "orderInfo";
             string returnUrl = @"https://localhost:44333/Manager/RoomRentalSlip";
@@ -75,5 +76,35 @@ namespace HotelManagement.Controllers
 
             return Redirect(jmessage.GetValue("payUrl").ToString());
         }
+
+        public ActionResult ReturnUrl()
+        {
+            string param = Request.QueryString.ToString().Substring(0, Request.QueryString.ToString().IndexOf("signature") - 1);
+            param = Server.UrlDecode(param);
+
+            MoMoSecurity crypto = new MoMoSecurity();
+
+            string secretKey = MomoConfig.SecretKey;
+
+            string signature = crypto.signSHA256(param, secretKey);
+
+            if(signature != Request["Signature"].ToString())
+            {
+                ViewBag.message = "Invalid Information";
+                return View();
+            }
+
+            if(!Request.QueryString["errorCode"].Equals("0"))
+            {
+                ViewBag.message = "Payment failed";
+            }
+            else
+            {
+                ViewBag.message = "Payment Success";
+            }
+
+            return View();
+        }
+
     }
 }
---
+++ HotelManagement/HotelManagement/Models/Dtos/MomoConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HotelManagement.Models.Dtos
+{
+    public static class MomoConfig
+    {
+        public static string ParnerCode = "MOMOGQOC20201207";
+
+        public static string AccessKey = "tb0AnQrtECJ3H0Zu";
+        public static string SecretKey = "2l9NSfk8rWqc4CpYGZva7dBfCYo9xM25";
+    }
+}
